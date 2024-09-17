9e5c7e8aa5e746bf95156fa2d3358f71f65bb43d
Kader1812 <hameedfocus7865@gmail.com>
2024-09-17 16:18:08 +0530

    My firts commit

---
+++ web1/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Brain
+
+
+
+class BrainSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+      model = Brain
+      fields = '__all__'
+      
+
